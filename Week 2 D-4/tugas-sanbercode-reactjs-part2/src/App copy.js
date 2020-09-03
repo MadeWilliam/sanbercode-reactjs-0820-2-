47ae5b33d88c9fb955e50eb784b7ff1c22623ae1
pupul.mwm <pupul.mwm@gmail.com>
2020-09-04 07:21:30 +0800

    Tugas 9 DONE

---
+++ Week 2 D-4/tugas-sanbercode-reactjs-part2/src/App copy.js
@@ -0,0 +1,40 @@
+// import React from 'react';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div style={{ maxWidth: "400px", margin: "20px auto", padding: 0}}>
+//       <div style={{padding: "20px", border: "1px solid black", borderRadius: "15px"}}>
+//         <h1 style={{textAlign:"center"}}>Form Pembelian Buah</h1>
+//         <div>
+//           <div style={{display: "block", marginBottom: "1em"}}>
+//             <div style={{display: "inline-block", width: "150px", fontWeight: "bold", fontSize: "16px"}}>
+//               Nama Pelanggan
+// 		                </div>
+//             <input style={{display: "inline-block"}} type="text" name="name"/>
+// 		            </div>
+//             <div style={{display: "block", marginBottom: "1em"}}>
+//               <div style={{display: "inline-block", width: "150px", fontWeight: "bold", fontSize: "16px"}}>
+//                 Daftar Item
+//               </div>
+//               <div style={{display: "inline-block;"}}>
+//                 <input type="checkbox" id="semangka" name="gender" value="semangka"/>
+//                 <label for="semangka">Semangka</label><br/>
+//                 <input type="checkbox" id="jeruk" name="gender" value="jeruk"/>
+//                 <label for="jeruk">Jeruk</label><br/>
+//                 <input type="checkbox" id="nanas" name="gender" value="nanas"/>
+//                 <label for="nanas">Nanas</label><br/>
+//                 <input type="checkbox" id="salak" name="gender" value="salak"/>
+//                 <label for="salak">Salak</label><br/>
+//                 <input type="checkbox" id="anggur" name="gender" value="anggur"/>
+//                 <label for="anggur">Anggur</label><br/>
+//               </div>
+//             </div>
+//               <input type="submit" value="Kirim" style={{background: "white", border: "2px solid linearGradient(to right, red, purple); cursor: pointer; border-radius: 30px;"}}/>
+//         </div>
+//       </div>
+//     </div>
+//   	);
+// }
+
+// export default App;
